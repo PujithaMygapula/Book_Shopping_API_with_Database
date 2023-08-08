c60d4793b5f4784cf1e0e9d83bb8eebda40aeec9
Pujitha <126239312+PujithaMygapula@users.noreply.github.com>
2023-08-08 16:54:34 +0530

    Create setupdb.go

---
+++ Database/setupdb.go
@@ -0,0 +1,21 @@
+package Database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "puji1211"
+	DB_NAME     = "postgres"
+)
+
+// Database set up
+func SetupDB() *sql.DB {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, _ := sql.Open("postgres", dbinfo)
+	return db
+}
